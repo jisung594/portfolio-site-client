da1fe40e9328cbbf1bd7d4e734923e1ca69db494
Jonathan Choi <jisung594@gmail.com>
2019-07-18 22:05:01 -0400

    start of project cards

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -3,10 +3,24 @@ import React from 'react';
 const ProjectCard = (props) => {
   let { project } = props;
 
+  // let projectKeys = Object.keys(project)
+
+  let keyValues
+  if (project["type"] === "dev") {
+    keyValues = <div>
+      <h2>{project["name"]}</h2>
+      <a href={project["url"]}>VIEW</a>
+    </div>
+  } else {
+    keyValues = <div>
+      <h2>{project["name"]}</h2>
+      <h5>{project["img"]}</h5>
+    </div>
+  }
+
   return (
     <div>
-      <h3>{project["name"]}</h3>
-      <p>{project["url"]}</p>
+      {keyValues}
     </div>
   )
 }
