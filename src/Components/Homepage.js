5f1ec65543d53ce9cd62178da7310a985d13348a
Jonathan Choi <jisung594@gmail.com>
2019-07-25 22:22:13 -0400

    working?

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -6,7 +6,7 @@ const Homepage = () => {
     <div>
       <h1>Hi!</h1>
       <h1>I'm Jonathan.</h1>
-      <h1>Welcome to my (work in progress) portfolio.</h1>
+      <h2>Welcome to my (work in progress) portfolio.</h2>
       <h1>Thanks for coming.</h1>
     </div>
   )
