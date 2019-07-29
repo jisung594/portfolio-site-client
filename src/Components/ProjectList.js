386270c31bff61ff4377843245651d6611199590
Jonathan Choi <jisung594@gmail.com>
2019-07-29 18:13:59 -0400

    working?

--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -8,7 +8,7 @@ const ProjectList = (props) => {
 
   let keyValues
   if (project["type"] === "dev") {
-    keyValues = <div className="dev-container">
+    keyValues = <div className="dev-card">
       <img src={project["img"]} alt="screenshot" crossOrigin=""/>
       <div className="project-info">
         <h2>{project["name"]}</h2>
@@ -16,7 +16,7 @@ const ProjectList = (props) => {
       </div>
     </div>
   } else {
-    keyValues = <div className="design-container">
+    keyValues = <div className="design-card">
       <img src={project["img"]} alt="screenshot" crossOrigin=""/>
       <div className="project-info">
         <h2>{project["name"]}</h2>
@@ -31,7 +31,10 @@ const ProjectList = (props) => {
   // }
 
   return (
-    <div id="portfolio-list">
+    <div id="portfolio-list" style={
+      project["type"] == "dev" ? "display: grid;";
+      : "display: grid; grid-template-columns: 30vw 30vw 30vw"
+    }>
       {keyValues}
       {/*downloadImage(project["img"])*/}
     </div>
