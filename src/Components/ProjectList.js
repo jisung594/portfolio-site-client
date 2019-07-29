30172624b90a9ff6366b8330d713ad867d2f1fd3
Jonathan Choi <jisung594@gmail.com>
2019-07-29 18:54:24 -0400

    working?

--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -33,7 +33,7 @@ const ProjectList = (props) => {
   return (
     <div id="portfolio-list" style={
       project["type"] === "dev" ? {display: "grid"}
-      : {display: "grid", grid-template-columns: "30vw 30vw 30vw"}
+      : {display: "grid"; grid-template-columns: "30vw 30vw 30vw";}
     }>
       {keyValues}
       {/*downloadImage(project["img"])*/}
