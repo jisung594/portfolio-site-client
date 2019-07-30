c4d6cc3f6417bc8a9b36d694086f384641bdb0d4
Jonathan Choi <jisung594@gmail.com>
2019-07-30 06:28:04 -0400

    site links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,6 +11,10 @@ const Contact = () => {
             <h1>CONTACT</h1>
             <h4>P: (978) 604-4087</h4>
             <h4>E: jonjichoi@gmail.com</h4>
+
+            <a href="https://www.linkedin.com/in/jonathanchoi594/">Linkedin</a>
+            <a href="https://github.com/jisung594">Github</a>
+            <a href="https://medium.com/@jisung594">Blog</a>
           </div>
         }
       }/>
