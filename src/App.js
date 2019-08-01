f907303eb52fd6db4b61a5e961fcba12b7051622
Jonathan Choi <jisung594@gmail.com>
2019-08-01 15:57:02 -0400

    heroku

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ class App extends Component {
   }
 
   callBackend = async () => {
-    // const response = await fetch('/api/projects')
-    const response = await fetch('https://jonathanchoi-api.herokuapp.com/api/projects')
+    const response = await fetch('/api/projects')
     const body = await response.json()
 
     if (response.status !== 200) {
