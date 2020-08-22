95d81eaf903ac1fa093f5d508ed503c5f573457e
Jonathan Choi <jisung594@gmail.com>
2020-08-22 19:40:06 -0400

    updated resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,7 +17,7 @@ const About = () => {
         </div>
       </div>
       <div>
-        <img id="resume" src="https://imgur.com/Jb25fNK" alt="resume"/>
+        <img id="resume" src="https://imgur.com/Jb25fNK.jpg" alt="resume"/>
       </div>
     </div>
   )
